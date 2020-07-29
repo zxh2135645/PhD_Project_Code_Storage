a8587433628a03852e225bac6a939bbffcbc27d3
James zhang <zxh2135645@gmail.com>
2020-07-29 16:07:39 -0700

    compatible with Mac

--- function/ExtractNames.m
+++ function/ExtractNames.m
@@ -2,7 +2,11 @@ function [Names] = ExtractNames(name_glob)
     Names = cell(length(name_glob), 1);
     
     for i = 1:length(Names)
-        strings = strsplit(name_glob{i}, '\');
+        if ispc % Check if operation system is windows
+            strings = strsplit(name_glob{i}, '\');
+        else
+            strings = strsplit(name_glob{i}, '/');
+        end
         Names{i} = strings{end-1};
     end
 end
