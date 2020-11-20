dca2c8b6d3375e816f56687da90768696a00bf34
James zhang <zxh2135645@gmail.com>
2020-11-19 17:53:15 -0800

    for reading spectrum

--- function/loadSVSData.m
+++ function/loadSVSData.m
@@ -1,8 +1,6 @@
 function ss = loadSVSData(dst_dir, SeriesNum, InstanceNum)
-    oxsa_dir = 'D:\OXSA\';
-    addpath(oxsa_dir);
+    addpath('../../OXSA/');
 
-    
     dt = Spectro.dicomTree('dir',dst_dir);
 
     matched = dt.searchForSeriesInstanceNumber(SeriesNum, InstanceNum);
