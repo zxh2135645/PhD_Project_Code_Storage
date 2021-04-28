14f851bbc6334018e73bb8992a65edc3c6012d65
James <zxh2135645@gmail.com>
2021-04-28 12:58:47 -0700

    Added lines to get value from ROI

--- LRT_ZH/supporting/recon/tensor_display_T1T2star.m
+++ LRT_ZH/supporting/recon/tensor_display_T1T2star.m
@@ -8,6 +8,21 @@ cw=max(vec(abs(temp)));
 h = implay(abs(temp(:,:,:,1))/cw);
 set(h.Parent,'Name','old_basal_echo1');
 
+%% Pick remote ROI for SNR analysis
+slc = 192;
+figure('Position', [100 100 1600 1000]); 
+imagesc(abs(temp(:,:,slc,1))); axis image;
+roi = drawpolygon;
+roi_pos = roi.Position;
+%%
+x = roi_pos(:,1);
+y = roi_pos(:,2);
+BW = poly2mask(x,y,192,192);
+%figure(); imagesc(BW)
+sig_mean = mean(nonzeros(BW.*abs(temp(:,:,slc,1))));
+sig_sd = std(nonzeros(BW.*abs(temp(:,:,slc,1))));
+
+SNR = sig_mean/sig_sd;
 %% resphape it into chronologic order
 tempp = reshape(permute(temp, [1 2 4 3]), Ny, Nx, []);
 h = implay(abs(tempp)/cw);
